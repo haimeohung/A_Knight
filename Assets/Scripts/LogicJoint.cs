494cb584a2748d4beb50e01169d0f52c35df13ca
vutuanhai237 <43202025+haimeohung@users.noreply.github.com>
2019-11-12 22:08:07 +0700

    FIX: fix folder game

---
+++ Assets/Scripts/LogicJoint.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Extentison;
+
+[ExecuteInEditMode]
+public class LogicJoint : MonoBehaviour
+{
+    [SerializeField] private Transform target; // targer aim to
+    private enum AutoConfigureType { none, horizontal, vertical }
+    [SerializeField] private AutoConfigureType autoConfigure = AutoConfigureType.none;
+    [SerializeField] private bool _flipAngle = true;
+    private Transform child;
+    private float r1, r2, r1delta, r2delta;
+    public bool FlipAngle { get => _flipAngle; set => _flipAngle = value; }
+    public void TryFlipAngle() { _flipAngle = !FlipAngle; }
+
+
+    void Start()
+    {
+        child = transform.GetChild(0);
+        Transform endTranform = child.GetChild(0);
+
+        r1 = (child.position - transform.position).Magnitude2D();
+        r2 = (endTranform.position - child.position).Magnitude2D();
+        r1delta = -CaculateAngle(child.localPosition);
+        r2delta = -CaculateAngle(endTranform.localPosition);
+    }
+
+    void Update()
+    {
+        if (target is null)
+            return;
+
+        Vector3 distance = target.position - transform.position;
+        if (distance.Magnitude2D() < 0.000001) // too close
+            return;
+        if (distance.Magnitude2D() > (r1+r2)) // too far
+        {
+            float angle = CaculateAngle(distance);
+            transform.rotation = Quaternion.Euler(0, 0, angle + r1delta);
+            child.rotation = Quaternion.Euler(0, 0, angle + r2delta);
+            return;
+        }
+        // making triangle
+        float angle1 = CaculateAngle(distance.magnitude, r1, r2);
+        float angle2 = CaculateAngle(r1, r2, distance.magnitude);
+
+        switch (autoConfigure)
+        {
+            case AutoConfigureType.vertical:
+                FlipAngle = target.position.x > transform.position.x;
+                break;
+            case AutoConfigureType.horizontal:
+                FlipAngle = target.position.y > transform.position.y;
+                break;
+        }
+
+        if (FlipAngle)
+        {
+            angle1 = -angle1;
+            angle2 = -angle2;
+        }
+        angle1 += CaculateAngle(distance);
+        angle2 += angle1 - 180;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle1 + r1delta);
+        child.rotation = Quaternion.Euler(0, 0, angle2 + r2delta);
+    }
+
+    float CaculateAngle(Vector3 vector) => Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+    float CaculateAngle(float a, float b, float c) => Mathf.Acos((a * a + b * b - c * c) / (2 * a * b)) * Mathf.Rad2Deg; // angle between edge a and b
+}
---
+++ Assets/Scripts/extension.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unity.Extentison
+{
+    public static class MyExtensions
+    {
+        public static float Magnitude2D(this Vector3 vector) => Mathf.Sqrt((vector.x * vector.x) + (vector.y * vector.y));
+        public static bool isNull(this Transform transform) => transform is null || transform.Equals(null);
+    }
+}
