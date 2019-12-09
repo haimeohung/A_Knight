29b683eae00733a96fba32608115d1aa0fcb0c8f
vutuanhai237 <43202025+vutuanhai237@users.noreply.github.com>
2019-12-09 21:33:47 +0700

    Add script enemy_follow

---
+++ Assets/Scripts/Enemy/Enemy_Follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Follow : MonoBehaviour
+{
+    public float speed;
+    public float detect_distance;
+    public float stop_distance;
+    private Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, target.position);
+        if (distance > stop_distance && distance < detect_distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
